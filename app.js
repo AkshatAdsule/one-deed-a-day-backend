568bf22f1bf6b4464f2e96b3f5a6bf8432ca3c2a
akshatadsule <akshat.adsule@gmail.com>
2020-08-17 14:46:37 -0700

    add login route

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo') (session);
 
 mongoose.connect(`mongodb+srv://admin:${process.env.ATLAS_KEY}@one-deed-a-day.iudkb.mongodb.net/one-deed-a-day?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useCreateIndex: true
 });
 
 const userSchema = mongoose.Schema({
@@ -34,7 +38,21 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+app.use(cookieParser())
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({
+        mongooseConnection: mongoose.connection
+    })
+}));
 app.set('view engine', 'ejs');
+
+// Utility routes
+app.get('/_info', function(req, res) {
+    res.send(req.session);
+})
 
 app.get('/', function (req, res) {
     res.render('about');
@@ -51,15 +69,41 @@ app.post('/register', function (req, res) {
                 email: req.body.email,
                 username: req.body.username,
                 password: hash
-            }, function (err) {
+            }, function (err, newUser) {
                 if (!err) {
+                    req.session.username = newUser.username;
                     res.redirect('/');
                 }
             });
         }
     });
 
-})
+});
+
+
+app.get('/login', function(req, res) {
+    res.render('login');
+});
+
+app.post('/login', function(req, res) {
+    User.findOne({
+        email: req.body.email
+    }, function(dbErr, user) {
+        if(!dbErr) {
+            bcrypt.compare(req.body.password, user.password, function(hashErr, same) {
+                if(!hashErr && same) {
+                    req.session.username = user.username;
+                    res.send('logged in');
+                } else {
+                    res.send(hashErr)
+                }
+            });
+        } else {
+            res.send(dbErr);
+        }
+        
+    });
+});
 
 app.listen(7000, function () {
     console.log('Server is running on 7000');
